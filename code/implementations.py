5eb69bf59cd8a4fa4f449346057218edae165727
Can Altıniğne <canaltinigne@gmail.com>
2018-10-15 23:57:24 +0200

    new methods added to implementations.py

--- code/implementations.py
+++ code/implementations.py
@@ -82,3 +82,184 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
         loss = compute_loss(y, tx, w, t='logistic', lambda_)
 
     return (w, loss)
+
+def split_data(x, y, ratio, seed=1):
+    np.random.seed(seed)
+    element_num = int(np.floor(ratio * len(y)))
+    random_ind = np.random.permutation(len(y))
+    
+    ind_train = random_ind[:element_num]
+    ind_test = random_ind[element_num:]
+    # create split
+    x_tr = x[ind_train]
+    x_te = x[ind_test]
+    y_tr = y[ind_train]
+    y_te = y[ind_test]
+    return x_tr, y_tr, x_te, y_te
+
+def build_poly(x, degree):
+    temp_arr = np.ones(len(x))
+    for i in range(1, degree+1):
+        arr = np.power(x, i)
+        temp_arr = np.column_stack((temp_arr, arr))
+        
+    return temp_arr
+
+def cross_validation(y, x, k_indices, k, lambda_, degree):
+    """return the loss of ridge regression."""
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # get k'th subgroup in test, others in train: TODO
+    # ***************************************************
+    X_test, y_test = x[k_indices[k]], y[k_indices[k]]
+    X_train = x[k_indices[[x for x in range(len(k_indices)) if x != k]]].ravel() 
+    y_train = y[k_indices[[x for x in range(len(k_indices)) if x != k]]].ravel()
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # form data with polynomial degree: TODO
+    # ***************************************************
+    X_train_ = build_poly(X_train, degree)
+    X_test_ = build_poly(X_test, degree)
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # ridge regression: TODO
+    # ***************************************************
+    w_ = ridge_regression(y_train, X_train_, lambda_)
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # calculate the loss for train and test data: TODO
+    # ***************************************************
+    loss_tr = np.sqrt(2*compute_mse(y_train, X_train_, w_))
+    loss_te = np.sqrt(2*compute_mse(y_test, X_test_, w_))
+    return loss_tr, loss_te
+
+def build_k_indices(y, k_fold, seed):
+    """build k indices for k-fold."""
+    num_row = y.shape[0]
+    interval = int(num_row / k_fold)
+    np.random.seed(seed)
+    indices = np.random.permutation(num_row)
+    k_indices = [indices[k * interval: (k + 1) * interval]
+                 for k in range(k_fold)]
+    return np.array(k_indices)
+
+def cross_validation_demo():
+    seed = 1
+    degree = 7
+    k_fold = 4
+    lambdas = np.logspace(-4, 0, 30)
+    # split data in k fold
+    k_indices = build_k_indices(y, k_fold, seed)
+    # define lists to store the loss of training data and test data
+    rmse_tr = []
+    rmse_te = []
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # cross validation: TODO
+    # ***************************************************
+    for lambda_ in lambdas:
+        rmse_tr_tmp = []
+        rmse_te_tmp = []
+        for k in range(k_fold):
+            loss_tr, loss_te, = cross_validation(y, x, k_indices, k, lambda_, degree)
+            rmse_tr_tmp.append(loss_tr)
+            rmse_te_tmp.append(loss_te)
+            
+        rmse_tr.append(np.mean(rmse_tr_tmp))
+        rmse_te.append(np.mean(rmse_te_tmp))
+
+def bias_variance_demo():
+    """The entry."""
+    # define parameters
+    seeds = range(100)
+    num_data = 10000
+    ratio_train = 0.005
+    degrees = range(1, 10)
+    
+    # define list to store the variable
+    rmse_tr = np.empty((len(seeds), len(degrees)))
+    rmse_te = np.empty((len(seeds), len(degrees)))
+    
+    for index_seed, seed in enumerate(seeds):
+        np.random.seed(seed)
+        x = np.linspace(0.1, 2 * np.pi, num_data)
+        y = np.sin(x) + 0.3 * np.random.randn(num_data).T
+        # ***************************************************
+        # INSERT YOUR CODE HERE
+        # split data with a specific seed: TODO
+        # ***************************************************
+        X_train, y_train, X_test, y_test = split_data(x, y, ratio_train, seed)
+        # ***************************************************
+        # INSERT YOUR CODE HERE
+        # bias_variance_decomposition: TODO
+        # ***************************************************
+        for index_degree, degree in enumerate(degrees):
+            X_train_ = build_poly(X_train, degree)
+            X_test_ = build_poly(X_test, degree)
+            # least square
+            w = least_squares(y_train, X_train_)
+            # calculate the rmse for train and test
+            rmse_tr[index_seed, index_degree] = np.sqrt(2 * compute_mse(y_train, X_train_, w))
+            rmse_te[index_seed, index_degree] = np.sqrt(2 * compute_mse(y_test, X_test_, w))
+
+def polynomial_regression():
+    """Constructing the polynomial basis function expansion of the data,
+       and then running least squares regression."""
+    # define parameters
+    degrees = [1, 3, 7, 12]
+    
+    # define the structure of the figure
+    num_row = 2
+    num_col = 2
+    f, axs = plt.subplots(num_row, num_col)
+
+    for ind, degree in enumerate(degrees):
+        # ***************************************************
+        # INSERT YOUR CODE HERE
+        # form the data to do polynomial regression.: TODO
+        # ***************************************************
+        x_ = build_poly(x, degree)
+        # ***************************************************
+        # INSERT YOUR CODE HERE
+        # least square and calculate RMSE: TODO
+        # ***************************************************
+        mse, weights = least_squares(y, x_)
+        rmse = np.sqrt(2*mse)
+
+        print("Processing {i}th experiment, degree={d}, rmse={loss}".format(
+              i=ind + 1, d=degree, loss=rmse))
+        # plot fit
+        plot_fitted_curve(
+            y, x, weights, degree, axs[ind // num_col][ind % num_col])
+    plt.tight_layout()
+    plt.savefig("visualize_polynomial_regression")
+    plt.show()
+
+def train_test_split_demo(x, y, degree, ratio, seed):
+    """polynomial regression with different split ratios and different degrees."""
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # split the data, and return train and test data: TODO
+    # ***************************************************
+    train_x, train_y, test_x, test_y = split_data(x, y, ratio, seed)
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # form train and test data with polynomial basis function: TODO
+    # ***************************************************
+    train_poly = build_poly(train_x, degree)
+    test_poly = build_poly(test_x, degree)
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # calcualte weight through least square.: TODO
+    # ***************************************************
+    mse_tr, weights = least_squares(train_y, train_poly)
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # calculate RMSE for train and test data,
+    # and store them in rmse_tr and rmse_te respectively: TODO
+    # ***************************************************
+    rmse_tr = np.sqrt(2*mse_tr)
+    rmse_te = np.sqrt(2*calculate_mse(test_y - np.dot(test_poly, weights)))
+    
+    print("proportion={p}, degree={d}, Training RMSE={tr:.3f}, Testing RMSE={te:.3f}".format(
+          p=ratio, d=degree, tr=rmse_tr, te=rmse_te))
