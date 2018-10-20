76b0e06444d69d00a980d1d95d06240469d30dad
Can Altıniğne <canaltinigne@gmail.com>
2018-10-20 13:59:05 +0200

    Ridge regression used with cv

--- code/cross_validation.py
+++ code/cross_validation.py
@@ -1,65 +1,64 @@
 import numpy as np
+from implementations import ridge_regression
+from proj1_helpers import predict_labels
+from errors import log_loss
 
-def build_k_indices(y, k_fold, seed):
+def build_k_indices(y, k_fold, seed=23):
     """build k indices for k-fold."""
     num_row = y.shape[0]
     interval = int(num_row / k_fold)
     np.random.seed(seed)
     indices = np.random.permutation(num_row)
+
     k_indices = [indices[k * interval: (k + 1) * interval]
                  for k in range(k_fold)]
+
     return np.array(k_indices)
 
-def cross_validation(y, x, k_indices, k, lambda_, degree):
+def split_cross_validation(y, x, k_indices, k):
     """return the loss of ridge regression."""
     # ***************************************************
     # INSERT YOUR CODE HERE
     # get k'th subgroup in test, others in train: TODO
     # ***************************************************
-    X_test, y_test = x[k_indices[k]], y[k_indices[k]]
-    X_train = x[k_indices[[x for x in range(len(k_indices)) if x != k]]].ravel() 
-    y_train = y[k_indices[[x for x in range(len(k_indices)) if x != k]]].ravel()
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # form data with polynomial degree: TODO
-    # ***************************************************
-    X_train_ = build_poly(X_train, degree)
-    X_test_ = build_poly(X_test, degree)
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # ridge regression: TODO
-    # ***************************************************
-    w_ = ridge_regression(y_train, X_train_, lambda_)
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # calculate the loss for train and test data: TODO
-    # ***************************************************
-    loss_tr = np.sqrt(2*compute_mse(y_train, X_train_, w_))
-    loss_te = np.sqrt(2*compute_mse(y_test, X_test_, w_))
-    return loss_tr, loss_te
+    X_test, y_test = x[k_indices[k],:], y[k_indices[k]]
+    X_train = x[k_indices[[x for x in range(len(k_indices)) if x != k]]].reshape(-1,15)
+    y_train = y[k_indices[[x for x in range(len(k_indices)) if x != k]]].reshape(-1,1)
 
+    return X_train, y_train, X_test, y_test
 
-def cross_validation_demo():
-    seed = 1
-    degree = 7
-    k_fold = 4
-    lambdas = np.logspace(-4, 0, 30)
-    # split data in k fold
-    k_indices = build_k_indices(y, k_fold, seed)
-    # define lists to store the loss of training data and test data
-    rmse_tr = []
-    rmse_te = []
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # cross validation: TODO
-    # ***************************************************
-    for lambda_ in lambdas:
-        rmse_tr_tmp = []
-        rmse_te_tmp = []
-        for k in range(k_fold):
-            loss_tr, loss_te, = cross_validation(y, x, k_indices, k, lambda_, degree)
-            rmse_tr_tmp.append(loss_tr)
-            rmse_te_tmp.append(loss_te)
+def accuracy(pred, y):
+    counter = 0
+    for i in range(len(pred)):
+        if pred[i] == y[i]:
+            counter+=1
             
-        rmse_tr.append(np.mean(rmse_tr_tmp))
-        rmse_te.append(np.mean(rmse_te_tmp))
+    return counter/len(pred)
+
+def cross_validation(y, X, fold, lambdas, thresholds=[0], model='ridge', seed=23):
+
+    k_indices = build_k_indices(y, fold, seed)
+    # define lists to store the loss of training data and test data
+    accuracy_tr = []
+    accuracy_te = []
+
+    if model == 'ridge':
+        for thr in thresholds:
+            for lambda_ in lambdas:
+                train_err = []
+                test_err = []
+                
+                for k in range(fold):
+                    X_train, y_train, X_valid, y_valid = split_cross_validation(y, X, k_indices, k)
+                    w, _ = ridge_regression(y_train, X_train, lambda_)
+        
+                    pred_tr_y = predict_labels(w, X_train, thr)
+                    pred_te_y = predict_labels(w, X_valid, thr)
+
+                    train_err.append(accuracy(pred_tr_y, y_train))
+                    test_err.append(accuracy(pred_te_y, y_valid))
+               
+                accuracy_tr.append(np.array([thr, lambda_, np.mean(train_err)]))
+                accuracy_te.append(np.array([thr, lambda_, np.mean(test_err)]))
+
+    return np.array(accuracy_tr), np.array(accuracy_te)
--- code/errors.py
+++ code/errors.py
@@ -7,16 +7,15 @@ def calculate_mse(e):
 def calculate_mae(e):
     return np.mean(np.abs(e))
 
-def calculate_logistic_loss(y, tx, w, lambda_):
-    pred = sigmoid(np.dot(tx, w))
-    return -np.mean(y*np.log(pred) + (1-y)*np.log(1-pred)) + lambda_*np.dot(w.T, w)
+def log_loss(y, pred):
+    return -np.mean(y*np.log(pred) + (1-y)*np.log(1-pred)) + lambda_*np.dot(w.T, w) #Regularized
 
 def compute_loss(y, tx, w, t='mse', lambda_=0):
     e = y - np.dot(tx, w)
 
     if t == 'mae':
         return calculate_mae(e)
-    elif t == 'logistic':
-        return calculate_logistic_loss(y, tx, w, lambda_)
+    #elif t == 'log':
+    #    return log_loss(y, tx, w, lambda_)
     else:
         return calculate_mse(e)
