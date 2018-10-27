fb923c1b4314d5b26c9498ed1e4cececa7567a94
Can Altıniğne <canaltinigne@gmail.com>
2018-10-28 01:25:58 +0200

    Latest vers

--- code/cross_validation.py
+++ code/cross_validation.py
@@ -2,6 +2,7 @@ import numpy as np
 from implementations import *
 from proj1_helpers import predict_labels
 from errors import compute_loss
+from polynomial import build_poly
 
 def build_k_indices(y, k_fold, seed=23):
     """build k indices for k-fold."""
@@ -63,40 +64,56 @@ def cross_validation(y, X, fold, h_pars={}, model='ridge', seed=23):
                 accuracy_te.append(np.array([thr, lambda_, np.mean(test_err)]))
 
     elif model == 'least':
+        for degree in h_pars['degrees']:
+            
+            train_err = []
+            test_err = []
         
-        train_err = []
-        test_err = []
-        
-        for k in range(fold):
-            X_train, y_train, X_valid, y_valid = split_cross_validation(y, X, k_indices, k)
-            w, _ = least_squares(y_train, X_train)
+            for k in range(fold):
+                X_train, y_train, X_valid, y_valid = split_cross_validation(y, X, k_indices, k)
+                w, _ = least_squares(y_train, build_poly(X_train, degree))
 
-            pred_tr_y = predict_labels(w, X_train, thr)
-            pred_te_y = predict_labels(w, X_valid, thr)
+                pred_tr_y = predict_labels(w, build_poly(X_train, degree))
+                pred_te_y = predict_labels(w, build_poly(X_valid, degree))
 
-            train_err.append(accuracy(pred_tr_y, y_train))
-            test_err.append(accuracy(pred_te_y, y_valid))
-        
-        accuracy_tr.append(np.array([thr, np.mean(train_err)]))
-        accuracy_te.append(np.array([thr, np.mean(test_err)]))
+                train_err.append(accuracy(pred_tr_y, y_train))
+                test_err.append(accuracy(pred_te_y, y_valid))
+            
+            accuracy_tr.append(np.array([degree, np.mean(train_err)]))
+            accuracy_te.append(np.array([degree, np.mean(test_err)]))
 
     elif model == 'log':
-        initial_w = np.random.normal(0,1,X.shape[1])
-
-        for itr in h_pars['max_iter']:
-            for gamma in h_pars['gamma']:
+        for degree in h_pars['degrees']:
             
-                train_err = []
-                test_err = []
-                
-                for k in range(fold):
-                    X_train, y_train, X_valid, y_valid = split_cross_validation(real_y, X, k_indices, k)
-                    w, loss = logistic_regression(y_train, X_train, initial_w, itr, gamma)
+            np.random.seed(23)
+            initial_w = np.random.normal(0,1,build_poly(X, degree).shape[1]).reshape(-1,1)#
 
-                    train_err.append(loss)
-                    test_err.append(compute_loss(y_valid, X_valid, w, t='log'))
+            for itr in h_pars['max_iter']:
+                for gamma in h_pars['gamma']:
                 
-                accuracy_tr.append(np.array([itr, gamma, np.mean(train_err)]))
-                accuracy_te.append(np.array([itr, gamma, np.mean(test_err)]))
+                    train_err = []
+                    test_err = []
+                    train_acc = []
+                    test_acc = []
+                    
+                    for k in range(fold):
+                        X_train, y_train, X_valid, y_valid = split_cross_validation(y, build_poly(X, degree), k_indices, k)
+
+                        w, loss = logistic_regression(y_train, X_train, initial_w, itr, gamma)
+
+                        train_err.append(compute_loss(y_train, X_train, w, t='log'))
+                        test_err.append(compute_loss(y_valid, X_valid, w, t='log'))
+
+                        tr_ = predict_labels(w, X_train, model='log')
+                        te_ = predict_labels(w, X_valid, model='log')
+
+                        tr_[tr_ == -1] = 0
+                        te_[te_ == -1] = 0
+
+                        train_acc.append(accuracy(tr_, y_train))
+                        test_acc.append(accuracy(te_, y_valid))
+                    
+                    accuracy_tr.append(np.array([itr, gamma, degree, np.mean(train_err), np.mean(train_acc)]))
+                    accuracy_te.append(np.array([itr, gamma, degree, np.mean(test_err)], np.mean(test_acc)))
              
     return np.array(accuracy_tr), np.array(accuracy_te)
